27097bf28b80596fe86a765a08a4616f04a6a49a
Niki Kovacs <info@microlinux.fr>
2014-11-22 14:18:10 +0100

    ...

---
+++ tempsystem/16-diffutils.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Diffutils
+set -e
+WGETLIST="\
+http://ftp.gnu.org/gnu/diffutils/diffutils-3.3.tar.xz"
+cd $LFS/sources
+wget -c $WGETLIST
+rm -rf diffutils-3.3
+tar -xvf diffutils-3.3.tar.xz
+cd diffutils-3.3
+./configure --prefix=/tools
+make -j 5
+make check
+make install
+
