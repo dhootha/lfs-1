b2ae0fdf37e47008fdbc0dbffaa04ee72c1a0c1a
Niki Kovacs <info@microlinux.fr>
2014-11-23 17:43:20 +0100

    ...

--- tempsystem/11-check.sh
+++ tempsystem/11-check.sh
@@ -5,7 +5,7 @@ set -e
 WGETLIST="\
 http://sourceforge.net/projects/check/files/check/0.9.14/check-0.9.14.tar.gz"
 cd $LFS/sources
-#wget -c $WGETLIST
+wget -c $WGETLIST
 rm -rf check-0.9.14
 tar -xvf check-0.9.14.tar.gz
 cd check-0.9.14
--- tempsystem/28-tar.sh
+++ tempsystem/28-tar.sh
@@ -9,6 +9,7 @@ wget -c $WGETLIST
 rm -rf tar-1.28
 tar -xvf tar-1.28.tar.xz
 cd tar-1.28
+FORCE_UNSAFE_CONFIGURE=1 \
 ./configure --prefix=/tools 
 make -j 5
 #make check
