01aa0198efc9a04a6051ff23267744a5c9862991
Niki Kovacs <info@microlinux.fr>
2014-11-25 06:39:48 +0100

    ...

--- toolchain/build-toolchain.sh
+++ toolchain/build-toolchain.sh
@@ -1,38 +1,80 @@
 #!/bin/sh
 set -e
 CWD=$(pwd)
-$CWD/00-download.sh
-$CWD/01-binutils-pass-1.sh
-$CWD/02-gcc-pass-1.sh
-$CWD/03-linux-headers.sh
-$CWD/04-glibc.sh
-$CWD/05-libstdc++.sh
-$CWD/06-binutils-pass-2.sh
-$CWD/07-gcc-pass-2.sh
-$CWD/08-tcl.sh
-$CWD/09-expect.sh
-$CWD/10-dejagnu.sh
-$CWD/11-check.sh
-$CWD/12-ncurses.sh
-$CWD/13-bash.sh
-$CWD/14-bzip2.sh
-$CWD/15-coreutils.sh
-$CWD/16-diffutils.sh
-$CWD/17-file.sh
-$CWD/18-findutils.sh
-$CWD/19-gawk.sh
-$CWD/20-gettext.sh
-$CWD/21-grep.sh
-$CWD/22-gzip.sh
-$CWD/23-m4.sh
-$CWD/24-make.sh
-$CWD/25-patch.sh
-$CWD/26-perl.sh
-$CWD/27-sed.sh
-$CWD/28-tar.sh
-$CWD/29-texinfo.sh
-$CWD/30-util-linux.sh
-$CWD/31-xz.sh
-$CWD/32-strip.sh
-$CWD/33-virtual-filesystems.sh
-$CWD/34-chroot.sh
+export LFS=/mnt/lfs
+
+# Clean start
+if grep -q /mnt/lfs/boot /proc/mounts; then
+  umount /mnt/lfs/boot
+fi
+
+if grep -q /mnt/lfs /proc/mounts; then
+  umount /mnt/lfs
+fi
+
+if [[ $(swapon -s | grep -ci "/dev") -gt 0 ]]; then
+  swapoff /dev/sda2
+fi
+
+rm -f /tools
+
+# Create filesystems
+mkfs -v -t ext2 /dev/sda1
+mkswap /dev/sda2
+mkfs -v -t ext4 /dev/sda3
+
+# Mount filesystems
+mount -v -t ext4 /dev/sda3 $LFS
+mkdir -v $LFS/boot
+mount -v -t ext2 /dev/sda1 $LFS/boot
+
+# Activate swap partition
+swapon -v /dev/sda2
+
+# Placeholder for source archives and patches
+mkdir -v $LFS/sources
+
+# Toolchain programs will be installed here
+mkdir -v $LFS/tools
+ln -sv $LFS/tools /
+
+# Set up a good working environment
+cat > ~/.bash_profile << "EOF"
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+
+cat > ~/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/tools/bin:/usr/sbin:/sbin:/usr/bin:/bin
+export LFS LC_ALL LFS_TGT PATH
+# Colored output for 'ls'
+eval `dircolors -b`
+alias ls='ls --color=auto'
+
+# Some handy aliases
+alias ll='ls -al'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias cp='cp -i'
+alias rm='rm -i'
+alias mv='mv -i'
+alias vi='vim'
+
+# Coloured command prompt
+WHITE='\[\033[1;37m\]'
+RED='\[\033[0;33m\]'
+NC='\[\033[0m\]'
+PS1="$RED[$WHITE\u$NC@$WHITE\h$NC:$WHITE\W$RED] #$NC "
+
+# Vim is our preferred editor
+EDITOR=vim
+VISUAL=$EDITOR
+export EDITOR VISUAL
+EOF
+
+source ~/.bash_profile
+
